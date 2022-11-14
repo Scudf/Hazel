01f63349c84d18d7fb12de36321f321f81a55eed
scudf <scud.forward@gmail.com>
2022-11-15 01:06:14 +0200

    Removed extra file and code

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -7,8 +7,6 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
-	HZ_CORE_WARN("Hello World!");
-	HZ_INFO("My name is Vas!");
 
 	auto app = Hazel::CreateApplication();
 	app->run();
